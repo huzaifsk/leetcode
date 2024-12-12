c53ed69f22b56f86124e5ca7c4da3ea5aa8b37a0
Huzaif Shaikh <59112665+huzaifsk@users.noreply.github.com>
2024-12-12 13:29:13 +0530

    Time: 65 ms (90.11%), Space: 53.1 MB (36.59%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -10,7 +10,6 @@ var reverse = function(x) {
       let lastDigit = x % 10;
         revNum = (revNum * 10) + lastDigit
         x = Math.floor(x / 10);
-
         // Check if revNum is out of the 32-bit signed integer range
         if (revNum > Math.pow(2, 31) - 1) {
             return 0; // Return 0 if the number exceeds the range
