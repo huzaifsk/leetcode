5a586da5af71faf94518a87197c1edeff1c9d0b1
Huzaif Shaikh <59112665+huzaifsk@users.noreply.github.com>
2025-01-10 21:08:02 +0530

    Update 0009-palindrome-number.js

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,6 +2,36 @@
  * @param {number} x
  * @return {boolean}
  */
+
+var isPalindrome = function(s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        // Check if the left character is not alphanumeric
+        if (!isAlnum(s[left])) {
+            left++;
+        }
+        // Check if the right character is not alphanumeric
+        else if (!isAlnum(s[right])) {
+            right--;
+        }
+        // Compare characters after converting to lowercase
+        else if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+    return true;
+    
+};
+
+function isAlnum(char) {
+    return /^[a-zA-Z0-9]$/.test(char);
+}
+
+
 var isPalindrome = function(x) {
     let numCheck = x.toString().split('').reverse().join('');
     if(x == numCheck) return true;
